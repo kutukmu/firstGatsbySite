86e240e084777740a37d1215c43018372d62f6cc
Kerim <kutukmu@gmail.com>
2020-06-11 09:42:54 -0500

    changes

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import Head from "../components/head"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 
 
 const NotFound = () => {
